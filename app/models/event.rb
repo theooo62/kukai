bf07d24ddf1486a5394b35c20668435719b81efc
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-09 20:04:10 +0900

    モデル同士のアソシエーション記述(event--event_users--user)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+  has_many :user, through: :event_users
+  has_many :event_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :event, through: :event_users
+  has_many :event_users
 end
