897b8bac7ae98ebe5379b257bc4ee039da27f278
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-10 17:24:18 +0900

    日本語化（i18nロケールファイル）ダウンロード、ファイル作成　※未設定

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Kukai
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'devise.ja.yml', '*.{rb,yml}').to_s]
   end
 end
