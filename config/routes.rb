b7a2e1966926d11a33c46bf4c573ff8a04c7f5d3
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-12 02:00:53 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'haiku/new' => 'haiku#new' #新規画面
   # post 'haiku' => 'haiku#create' #投稿
   get 'users/:id' => 'users#show' #マイページ
-  get '/event/:event_id/haiku/:id/edit' => 'haiku#edit' #イベントページから俳句編集
+  # get '/event/:event_id/haiku/:id/edit' => 'haiku#edit' 下に書いてるから必要ない
 
   resources :event do
     resources :haiku ,only: [:create, :edit, :update, :destroy]
