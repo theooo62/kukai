91dbf584655ab95ea844a68d6f29935f36270073
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-12 01:48:27 +0900

    ルーティング追記　イベントページからの俳句編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'haiku/new' => 'haiku#new' #新規画面
   # post 'haiku' => 'haiku#create' #投稿
   get 'users/:id' => 'users#show' #マイページ
+  get '/event/:event_id/haiku/:id/edit' => 'haiku#edit' #イベントページから俳句編集
 
   resources :event do
     resources :haiku ,only: [:create, :edit, :update, :destroy]
