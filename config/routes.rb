9e1f55bd08eb6bddfd247c51df974919d4389101
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-24 15:00:01 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   root 'haiku#main' #一番最初の画面(予定)
   get 'haiku' => 'haiku#index'#俳句ランダム一覧
   get 'haiku/new' => 'haiku#new' #新規画面
-  # post 'haiku' => 'haiku#create' #投稿
   get 'users/:id' => 'users#show' #マイページ
-  # get '/event/:event_id/haiku/:id/edit' => 'haiku#edit' 下に書いてるから必要ない
-  # post '/event' => 'event#create' これもいらない？？
 
   resources :event do
     resources :haiku ,only: [:create, :edit, :update, :destroy]
