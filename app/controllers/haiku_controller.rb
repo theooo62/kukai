ab2eb332b087f199de003607c16567356b42df9c
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-24 10:57:14 +0900

    no message

--- app/controllers/haiku_controller.rb
+++ app/controllers/haiku_controller.rb
@@ -13,47 +13,8 @@ class HaikuController < ApplicationController
 	def new
 	end
 
-	def new_event_create #eventビューページ作る！！！！！
-		Event.create(event_params) #ストロングパラメーターじゃなくてもいい？
- 	end
-
-	def create #イベントページで俳句作成
-		Haiku.create(haiku_params)
-	end
-
-	def show #イベントページでの俳句一覧
-		haikus2 = Haiku.where(event_id) #where句でデータを絞る
-		@haikus_random2 = haikus2.sample(30)
-	end
-
-	def destroy　#イベントページから飛べるように！！！！
-		haiku = Haiku.find(params[:id])
-		if haiku.user_id == current_user.id
-		haiku.destroy
-		end
-	end
-
-	def edit #イベントページから飛べるように！！！！！
-    @haiku = Haiku.find(params[id])
-	end
-
-
-	def update
-		haiku = Haiku.find(params[:event_id]) #ここのハッシュはevent_userから考える？
-		if haiku.user_id == current_user.id
-			haiku.update(haiku_params)
-		end
-	end
 
 	private
-	def event_params
-		params.permit(:event_title)
-	end
-
-	def haiku_params
-		params.permit(:name, :text)
-	end
-
 	def move_to_main
       redirect_to action: :index unless user_signed_in?
 	end
