51db509f8f1f436908e87fc6872b8aed1a557ca3
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-11-26 17:48:31 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'haiku#main' #一番最初の画面(予定)
-  get 'haiku' => 'haiku#index'#俳句ランダム一覧
-  get 'haiku/new' => 'haiku#new' #新規画面
+  root 'event#index' #一番最初の画面(予定)#俳句ランダム一覧
   get 'users/:id' => 'users#show' #マイページ
 
   resources :event do
-    resources :haiku ,only: [:create, :edit, :update, :destroy]
+    resources :haiku ,only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
