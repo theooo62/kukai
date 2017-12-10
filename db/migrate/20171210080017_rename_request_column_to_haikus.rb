e7341a676462f03c00519687da832ae67463edf2
岩本由紀 <iwamotoyuki@iwamotoyukinoMacBook-Air.local>
2017-12-10 17:37:31 +0900

    マイグレーションファイルにメモ書き追記

--- db/migrate/20171210080017_rename_request_column_to_haikus.rb
+++ db/migrate/20171210080017_rename_request_column_to_haikus.rb
@@ -3,3 +3,7 @@ class RenameRequestColumnToHaikus < ActiveRecord::Migration
     rename_column :haikus, :haiku, :text
   end
 end
+
+#開発環境のデータベースのhaikusテーブルのカラム名だけ変わっていたので変更のため作成
+#ローカルで実行すると多分エラーになる
+#今後migration実行する時　注意
